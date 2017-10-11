1bb5d23fa3b68a78feafdaf4a974147a5cb857f7
Minh Nguyen <minh@mnguyen.io>
2017-10-11 23:36:54 +1100

    Reduce character maximum for string to 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // For utf8mb4 (MySQL/MariaDB), we need to reduce the maximum character length to 191
+        Schema::defaultStringLength(191);
     }
 
     /**
