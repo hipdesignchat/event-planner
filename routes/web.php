956ef554429336a91af675a69ce874822ced5c48
Minh Nguyen <minh@mnguyen.io>
2017-10-16 16:31:20 +1100

    Provide some routes to events

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('event', 'EventController@index');
+Route::get('/event', 'EventController@index');
+Route::get('/event/view/{event}', ['as' => 'event_view', 'uses' => 'EventController@show']);
+Route::get('/event/new', 'EventController@create');
+Route::post('/event', 'EventController@store');
