58fb699a686f006ba8c541dc9a0cc9a8d7434fcf
Minh Nguyen <minh@mnguyen.io>
2017-10-16 16:27:37 +1100

    Mark visible and fillable attributes of event

--- app/Event.php
+++ app/Event.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    protected $visible = ['title','description','location','status_id','event_begin','event_end'];
+
+    protected $fillable = ['title','description','location','owner_id','status_id','event_begin','event_end'];
+
     public function scopeCurrent($query) {
       return $query->where('status_id', 1);
     }
