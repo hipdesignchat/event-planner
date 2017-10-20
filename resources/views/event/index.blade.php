f60941221318ce934a17e1bd1ff2f68aa0daea1d
Minh Nguyen <minh@mnguyen.io>
2017-10-20 11:39:53 +1100

    Update event index to use the layout template

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta charset="utf-8">
-    <title>Event Index</title>
-</head>
-<body>
+@extends ('template/layout')
+@section ('content')
   <h1>All Events</h1>
   <ul>
     @foreach ($events as $event)
     <li><a href="/event/view/{{ $event->id }}">{{ $event->title }}</a></li>
     @endforeach
   </ul>
-</body>
-</html>
+@endsection
