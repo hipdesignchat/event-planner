dc6250a610ae627f46b24890c17c82debd179466
Minh Nguyen <minh@mnguyen.io>
2017-11-03 15:12:16 +1100

    Create event factory to generate events

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -4,6 +4,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Event::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->sentence,
+        'user_id' => function() {
+          return factory(App\User::class)->create()->id;
+        },
+        'description' => $faker->paragraph,
+        'location' => $faker->word,
+        'status_id' => 1,
+        'event_begin' => \Carbon\Carbon::now(),
+        'event_end' => \Carbon\Carbon::now()
     ];
 });
