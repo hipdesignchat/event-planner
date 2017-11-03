910c13fb13607e9e7f24025b6936f830e641563d
Minh Nguyen <minh@mnguyen.io>
2017-11-03 15:11:10 +1100

    Use blade title, use generated css, replace template.header with template.nav

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -4,10 +4,11 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Document</title>
+  <title>{{ $title }}</title>
+  <link type="text/css" rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-  @include ('template.header')
+  @include ('template.nav')
   
   @if (count($errors))
     @foreach ($errors->all() as $error)
