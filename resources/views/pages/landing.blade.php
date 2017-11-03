09fdbacfeed23ab7786670beb43d44b69b30130f
Minh Nguyen <minh@mnguyen.io>
2017-11-03 15:14:34 +1100

    Assign landing class to hero

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -1,6 +1,6 @@
 @extends ('template/layout')
 @section ('content')
-  <section>
+  <section class="landing">
     <h1>Do something</h1>
     <a href="register" class="button">Sign Up</a>
   </section>
