2df05ff28e1f58335c8abea252f3958e9b4e1219
Minh Nguyen <minh@mnguyen.io>
2017-11-08 22:47:34 +1100

    Fix missing semicolon

--- database/migrations/2017_11_06_052539_create_categories_table.php
+++ database/migrations/2017_11_06_052539_create_categories_table.php
@@ -17,7 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('description');
-            $table->string('slug')
+            $table->string('slug');
             $table->timestamps();
         });
     }
