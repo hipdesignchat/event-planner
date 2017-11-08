dde26878e030663ba15a521aac5671ea45087d06
Minh Nguyen <minh@mnguyen.io>
2017-11-08 18:25:02 +1100

    Echo csrf_token on every page

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ $title }}</title>
   <link type="text/css" rel="stylesheet" href="/css/app.css">
   <script type="text/javascript" src="/js/app.js" async></script>
