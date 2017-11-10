39dcb58c6334b06a1ac2de66032f253bba1c9fe7
jamespsp <31334953+jamespsp@users.noreply.github.com>
2017-11-10 14:53:12 +1100

    Update footer.blade.php

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,4 +1,5 @@
 <!-- Footer -->
+  <section data-sr class="footer u-full-width">
   <div class="container">
   	<div class="row">
   		<div class="three columns">
@@ -27,7 +28,9 @@
 		
     </div>
     <div class="row">
+    	<div class="twelve columns>
     <h5>&copy; 2017 Hip Design</h5>
+	</div>    
     </div>
-	</div>
+   </div>
 </section>
