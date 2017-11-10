aac03856288ab07c8e980c5eb97ef00af66c65c2
jamespsp <31334953+jamespsp@users.noreply.github.com>
2017-11-10 14:41:14 +1100

    Update footer.blade.php

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,10 +1,33 @@
 <!-- Footer -->
-<section data-sr class="footer u-full-width">
   <div class="container">
+  	<div class="row">
+  		<div class="three columns">
+  			<h5> Create an Event </h5>
+  		</div>
+  		<div class="offset-by-six three columns ">
+  			<h5> Log In </h5>
+  		</div>	
+  	</div>
     <div class="row">
-      <div class="twelve columns">
-        <h5>&copy; 2017 Hip Design</h5>
-      </div>
+      <div class="three columns">
+      	<ul>
+      	<li> Help </li>
+      	<li> Guidelines </li>
+      	<li> Events by City </li>
+      	<li> About Us </li>
+      </ul>
+  	  </div>
+      <div class="three columns">
+      	<ul>
+      	<li> Terms and Conditions </li>
+      	<li> Privacy Policy </li>
+      	</ul>
+		</div>
+
+		
     </div>
-  </div>
+    <div class="row">
+    <h5>&copy; 2017 Hip Design</h5>
+    </div>
+	</div>
 </section>
