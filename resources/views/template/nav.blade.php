398ab3cfdd1dfd2dc14ba3f031f1657f61b98add
jamespsp <31334953+jamespsp@users.noreply.github.com>
2017-11-10 16:35:52 +1100

    Update nav.blade.php

--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -1,7 +1,7 @@
 <header>
   <nav class="navigation" style="width: 100%">
     <ul class="main-navigation" style="margin-bottom: 0!important; background-color: black;">
-      <li><img src="images/event.png"></li>
+      <li><img src="/images/event.png"></li>
       <li><a href="#introduction">Introduction</a></li>
       <li><a href="#categories">Categories</a></li>
       <li><a href="#contact">Contact</a></li>
