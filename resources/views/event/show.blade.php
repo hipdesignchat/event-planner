d5e6e4209116a6c3c568cfc9947f58365106a1c3
Minh Nguyen <minh@mnguyen.io>
2017-11-24 00:41:35 +1100

    Display event information where possible

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -1,5 +1,19 @@
-@extends ('template/layout')
-@section ('content')
+@extends ('template/two-columns')
+@section ('hero_background')
+{{ $event->colour }}
+@endsection
+@section ('hero_content')
   <h1>{{ $event->title }}</h1>
-  <h2>{{ $event->event_begin->diffForHumans() }}</h2>
+  <p>{{ $event->event_begin->format('F j Y\, g:i A') }}–{{ $event->event_end->format('F j Y\, g:i A') }}</p>
+@endsection
+@section ('column_one')
+{{ $event->description }}
+@endsection
+@section ('column_two')
+<div class="register_interest">
+  <h1>Register your interest</h1>
+</div>
+<div class="location">
+  <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAvBA4fOryvPSVytsHSlJZ45yh5y3j2xOs&q=<?= urlencode($event->location) ?>" frameborder="0"></iframe>
+</div>
 @endsection
