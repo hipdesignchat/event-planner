fe96f1828007cc454b5e513844eed3717c59f3bb
Minh Nguyen <minh@mnguyen.io>
2017-11-23 23:07:19 +1100

    Separate site title from page title

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ $title }}</title>
+  <title>{{ $title }} :: Event Planner</title>
   <link type="text/css" rel="stylesheet" href="/css/app.css">
   <script type="text/javascript" src="/js/app.js" async></script>
 </head>
