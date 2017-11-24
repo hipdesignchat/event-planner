a58b1b848a69d76b3637c3582f3c3ed1130add42
Minh Nguyen <minh@mnguyen.io>
2017-11-24 14:11:02 +1100

    Get to user menu from name in nav

--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -6,7 +6,7 @@
       <li><a href="#contact">Contact</a></li>
       <li><a href="/about">About</a></li>
       @if (Auth::check())
-        <li><a href="#">{{ Auth::user()->name }}</a></li>
+        <li><a href="/user">{{ Auth::user()->name }}</a></li>
       @else
         <li><a href="/register">Sign Up</a></li>
         <li><a href="/login">Sign In</a></li>
